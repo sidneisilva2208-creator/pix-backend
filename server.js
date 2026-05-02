87e000fdb1721d8d02a41a0bfd390237f053f664
sidneisilva2208-creator <sidnei.silva2208@gmail.com>
2026-05-01 22:07:35 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import fetch from 'node-fetch';
+
+const app = express();
+app.use(express.json());
+
+app.post('/pix', async (req, res) => {
+  try {
+    const valor = req.body.valor;
+
+    const response = await fetch('https://expaybrasil.com/en/purchase/link', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        merchant_key: process.env.EXPAY_KEY,
+        currency_code: 'BRL',
+        amount: valor
+      })
+    });
+
+    const data = await response.json();
+
+    res.json({ copiaECola: data.emv });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ erro: 'Erro interno' });
+  }
+});
+
+app.listen(3000, () => console.log("Servidor rodando"));
